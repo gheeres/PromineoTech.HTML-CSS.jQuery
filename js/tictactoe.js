38502b0ce59b1ab9ee8120cb995980eadf135bab
George Heeres <gheeres@gmail.com>
2023-06-15 11:15:29 -0500

    Completed / working solution

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,5 +2,23 @@
   'use strict'
   
   $(document).ready(function() {
+    const $game = $('#game');
+
+    $game.on('click', '.cell', (e) => {
+      const $cell = $(e.target).closest('.cell');
+      if ($cell.length) {
+        const value = $cell.data('player') || 'X';
+        
+        $cell.empty();
+        if (value === 'O') {
+          $cell.append('<i class="bi bi-x-lg"></i>');
+          $cell.data('player', 'X');
+        }
+        else {
+          $cell.append('<i class="bi bi-circle"></i>');
+          $cell.data('player', 'O');
+        }
+      }
+    });
   });  
 })(jQuery);
